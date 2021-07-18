8b48e8a6b927998973b4ecd2571d7648550e857b
Sideshow-Charles <charlesonuoha97@gmail.com>
2021-07-18 08:22:59 +0100

    UI Features

--- pages/about/index.vue
+++ pages/about/index.vue
@@ -3,7 +3,8 @@
     <h1>About FDT!</h1>
     <p>From The Touchline aka FDT is a sports blog available and accesible to every user around the world!</p>
 
-    <p>Doesn't matter what you want to do, Read? Write? On FDT anyone can send a piece and have your article published so anyone around the world can read and digest your sports thoughts.</p>
+    <p>Doesn't matter what you want to do, Read? Write? On FDT anyone can send a piece and have your article published so anyone around the world can read and digest your sports thoughts.
+    </p>
 </div>
 </template>
 
